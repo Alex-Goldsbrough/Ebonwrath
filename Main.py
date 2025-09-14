a4dff87b9d8bfcdaee7a66cf0462685d92008ff7
Alex Goldsbrough <49494112+Alex-Goldsbrough@users.noreply.github.com>
2025-09-14 20:00:38 +0100

    v0.1.1
    Fixes.

--- Main.py
+++ Main.py
@@ -1,2 +1,31 @@
 #Designed and created by Alex Goldsbrough
 
+####################################################################
+
+#Room Imports
+import Rooms.Altar
+import Rooms.Armory
+import Rooms.Barracks
+import Rooms.Boss
+import Rooms.Catacombs
+import Rooms.Cells
+import Rooms.Chapel
+import Rooms.Crypt
+import Rooms.DeathScreen
+import Rooms.Dining
+import Rooms.Entrance
+import Rooms.Gate
+import Rooms.Guard_Post
+import Rooms.Kitchen
+import Rooms.Library
+import Rooms.Pantry
+import Rooms.Rune
+import Rooms.Shrine
+import Rooms.Storage
+import Rooms.Torture
+import Rooms.Trap
+import Rooms.WinScreen
+
+####################################################################
+
+
---
+++ Rooms/Entrance.py
@@ -0,0 +1,3 @@
+class Enterannce:
+
+    pass
